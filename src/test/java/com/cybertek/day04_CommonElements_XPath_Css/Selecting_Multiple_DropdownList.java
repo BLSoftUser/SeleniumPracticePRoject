fe675f7a60896124ca9f6d4a27df8c5194e4f947
BLSoftUser <bluelaser.soft@gmail.com>
2021-10-01 22:55:50 -0400

    Day04 Selecting Multiple Elements from Dropdown List

---
+++ src/test/java/com/cybertek/day04_CommonElements_XPath_Css/Selecting_Multiple_DropdownList.java
@@ -0,0 +1,29 @@
+package com.cybertek.day04_CommonElements_XPath_Css;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Selecting_Multiple_DropdownList {
+    public static void main(String[] args) {
+    //1. setup driver and navigate to...
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        driver.manage().window().maximize();
+    //2. identify language Dropdown List
+        WebElement languagesDropD = driver.findElement(By.name("Languages"));
+        Select languagesSelectObj = new Select(languagesDropD);
+        //2.1. check if it multiple selection list
+        System.out.println("languagesSelectObj.isMultiple() = " + languagesSelectObj.isMultiple());
+        //2.2. select item
+        languagesSelectObj.selectByIndex(1);
+        languagesSelectObj.selectByValue("java");
+        languagesSelectObj.selectByVisibleText("Python");
+
+    driver.quit();
+    }
+}
