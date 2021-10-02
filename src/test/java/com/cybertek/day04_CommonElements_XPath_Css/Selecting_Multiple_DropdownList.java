1507be0cf03a17ed4c8de4f1b048fdca2b95ec65
BLSoftUser <bluelaser.soft@gmail.com>
2021-10-01 22:59:10 -0400

    Day04 Selecting Multiple Elements from Dropdown List

--- src/test/java/com/cybertek/day04_CommonElements_XPath_Css/Selecting_Multiple_DropdownList.java
+++ src/test/java/com/cybertek/day04_CommonElements_XPath_Css/Selecting_Multiple_DropdownList.java
@@ -14,15 +14,23 @@ public class Selecting_Multiple_DropdownList {
         WebDriver driver = new ChromeDriver();
         driver.get("http://practice.cybertekschool.com/dropdown");
         driver.manage().window().maximize();
+
     //2. identify language Dropdown List
         WebElement languagesDropD = driver.findElement(By.name("Languages"));
         Select languagesSelectObj = new Select(languagesDropD);
         //2.1. check if it multiple selection list
-        System.out.println("languagesSelectObj.isMultiple() = " + languagesSelectObj.isMultiple());
+            System.out.println("languagesSelectObj.isMultiple() = " + languagesSelectObj.isMultiple());
         //2.2. select item
-        languagesSelectObj.selectByIndex(1);
-        languagesSelectObj.selectByValue("java");
-        languagesSelectObj.selectByVisibleText("Python");
+            languagesSelectObj.selectByIndex(1);
+            languagesSelectObj.selectByValue("java");
+            languagesSelectObj.selectByVisibleText("Python");
+        //2.3 De-select items
+            languagesSelectObj.deselectByIndex(1);
+            languagesSelectObj.deselectByValue("java");
+            languagesSelectObj.deselectByVisibleText("Python");
+
+    //3. deselect all
+        languagesSelectObj.deselectAll();
 
     driver.quit();
     }
