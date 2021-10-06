2ee793e228f8eb5508ad9bc838e4531f68b18227
BLSoftUser <bluelaser.soft@gmail.com>
2021-10-06 18:27:03 -0400

    Day06 JUnit Practice  Login Form Practice Test

--- src/test/java/com/cybertek/Tests/day06_JUnit5_Practice/LoginForm_PracticeTest.java
+++ src/test/java/com/cybertek/Tests/day06_JUnit5_Practice/LoginForm_PracticeTest.java
@@ -1,62 +1,36 @@
 package com.cybertek.Tests.day06_JUnit5_Practice;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+    import io.github.bonigarcia.wdm.WebDriverManager;
+    import org.junit.jupiter.api.*;
+    import org.openqa.selenium.By;
+    import org.openqa.selenium.WebDriver;
+    import org.openqa.selenium.WebElement;
+    import org.openqa.selenium.chrome.ChromeDriver;
+    import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LoginForm_PracticeTest {
     WebDriver driver;     //1. Declare instance variable
-
-    @BeforeEach
-    public void setupDriver(){
+    @BeforeEach     public void setupDriver(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();    }
-
-    @AfterEach
-    public void closingBrowser(){
-        driver.quit();    }
-
-
-
-    @Test
-    public void testLogin(){
+    @AfterEach  public void closingBrowser(){   driver.quit();    }
+    @Test   public void testLogin(){
         driver.get("http://practice.cybertekschool.com/login");
-
-        //1. get the text from the opened page
+    //1. get the text from the opened page
         WebElement header = driver.findElement(By.xpath("//h2[ .='Login Page' ]"));
         System.out.println("header.getText() = " + header.getText());
-        //2. input username
+    //2. input username
         driver.findElement(By.cssSelector("input[name='username']")).sendKeys("tomsmith");
-        //3. input login
+    //3. input login
         driver.findElement(By.cssSelector("input[name='password']")).sendKeys("SuperSecretPassword");
-        //4. login Button click()
+    //4. login Button click()
         driver.findElement(By.id("wooden_spoon")).click();
-
-                // on the next page continue:
-        //5. get the text from the opened page
+    //5. on the next page continue: get the text from the opened page
         WebElement loggedInMessage = driver.findElement(By.xpath("//div[@id='flash']"));
-
-        //6. assertion:
+    //6. assertion:
         String expectedResult = "You logged into a secure area!";
-    //        assertEquals(expectedResult , loggedInMessage.getText());
+        //  assertEquals(expectedResult , loggedInMessage.getText());
             assertTrue(loggedInMessage.getText().startsWith(expectedResult));
-
-
-
-
-
-        //div[ contains( text() , 'You logged into a secure area!') ]
-        // use isDisplayed() method to check an element is displayed on the screen
+    //7. use isDisplayed() method to check an element is displayed on the screen
         System.out.println("loggedInMessage.isDisplayed() = " + loggedInMessage.isDisplayed());
-
-
-        //6. logout button click()
     }
 }
