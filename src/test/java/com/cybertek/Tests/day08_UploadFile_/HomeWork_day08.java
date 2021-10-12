db949b9499e73bbc6ea452926f27ed48c56ada27
BLSoftUser <bluelaser.soft@gmail.com>
2021-10-12 12:56:57 -0400

    Day 08. HomeWork_day08

---
+++ src/test/java/com/cybertek/Tests/day08_UploadFile_/HomeWork_day08.java
@@ -0,0 +1,21 @@
+package com.cybertek.Tests.day08_UploadFile_;
+
+public class HomeWork_day08 {
+
+    /**
+     * 1. navigate to http://practice.cybertekschool.com/dropdown and save current window handle into a variable.
+     * 2. Click on last "dropdown" to show all "options" (not a select dropdown keep in mind)
+     * 3. Hold down to (COMMAND on mac CONTROL on windows) and Click on Google Link then release it.
+     * 4. It will open the link in new tab
+     * 5. Click on the Dropdown Link again to open it.
+     * 6. Now Hold down to (COMMAND on mac CONTROL on windows) and Click on Amazon Link then release it.
+     * 7. It will open the link in another windows.
+     * 8. Repeat the same process for Facebook
+     * 9. Now you have 4 "window"s , Loop through them using their handle, and print out the title.
+     * 10. Optionally , if the title contains Facebook, Close the window|tab.
+     * 11. Optionally, if the title contains Amazon , Hover over to Prime from menu option (in between Best Sellers and Customer Service), it will show
+     * "Try Prime" link , Verify it isDisplayed.
+     * 12. Optionally, if the title contains Etsy , Scroll until the Enter Your Email input box is in the view.
+     * 13. These exercise can be done separately instead of switching to the window, do your best judgement to do them together or separately.
+     */
+}
