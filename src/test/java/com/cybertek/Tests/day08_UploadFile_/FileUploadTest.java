d32b813d09b1721083897db961906cad2bc9af5f
BLSoftUser <bluelaser.soft@gmail.com>
2021-10-12 12:15:03 -0400

    Day 08. FileUpload Practice

---
+++ src/test/java/com/cybertek/Tests/day08_UploadFile_/FileUploadTest.java
@@ -0,0 +1,23 @@
+package com.cybertek.Tests.day08_UploadFile_;
+
+import com.cybertek.utilities.TestBase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class FileUploadTest extends TestBase {
+    @Test
+    public void testFileUpload(){
+        //1. navigate to: http://practice.cybertekschool.com/upload
+        driver.get("http://practice.cybertekschool.com/upload");
+
+        //2. choose a file source for uploading
+        String FilePath1 = "C:\\Users\\Alex PC\\Desktop\\HTML_Class\\krasa2.jpg";
+
+        //3. identify "upload" box and send the FilePath1 file
+            driver.findElement(By.id("file-upload")).sendKeys(FilePath1);
+
+        //4. click() on Submit button
+            driver.findElement(By.id("file-submit")).click();
+
+    }
+}
